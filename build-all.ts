85064a0f78534bfe253e2bb9537081d5eeb0a0bb
Kai Niebes <kai.niebes@outlook.com>
2025-01-21 15:20:13 +0100

    Fix build order for ci:build by making hard requirements fixed

--- build-all.ts
+++ build-all.ts
@@ -1,24 +1,26 @@
-import { exec } from "node:child_process";
-import { readFile } from "node:fs/promises";
-import { promisify } from "node:util";
-import { getAngularJson, type AngularConfig } from "./helper-types";
+import { exec } from 'node:child_process';
+import { readFile } from 'node:fs/promises';
+import { promisify } from 'node:util';
+import { getAngularJson, type AngularConfig } from './helper-types';
+
+const hardRequirements = ['@kompakkt/extender', '@kompakkt/plugin-i18n'];
 
 const execPromise = promisify(exec);
 
 const getProjectNames = async () => {
   const angularJson = await getAngularJson();
-  return Object.keys(angularJson.projects).filter(
-    (p) => p !== "@kompakkt/extender"
-  );
+  return Object.keys(angularJson.projects).filter(p => !hardRequirements.includes(p));
 };
 
 const buildProject = async (projectName: string) => {
   console.log(`Building ${projectName}`);
-  return await execPromise(["ng", "build", projectName].join(" "));
+  return await execPromise(['ng', 'build', projectName].join(' '));
 };
 
 const buildAll = async () => {
-  await buildProject("@kompakkt/extender");
+  for (const hardRequirement of hardRequirements) {
+    await buildProject(hardRequirement);
+  }
 
   const projectNames = await getProjectNames();
   for (const projectName of projectNames) {
