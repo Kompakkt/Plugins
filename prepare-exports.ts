cd8b7bfa84537114ceeecc52a1e667d4254e423c
Kai Niebes <kai.niebes@outlook.com>
2026-03-19 11:49:50 +0100

    Remove plugin- prefix from exported names

--- prepare-exports.ts
+++ prepare-exports.ts
@@ -6,7 +6,7 @@ const main = async () => {
 
   const exports: Record<string, string> = {};
   for (const [name, project] of Object.entries(angularJson.projects)) {
-    exports[name.replace('@kompakkt/', './')] = './' + join(project.root, 'src/index.ts');
+    exports[name.replace('@kompakkt/', './').replace('plugin-', '')] = './' + join(project.root, 'src/index.ts');
   }
 
   const packageJson = await Bun.file('package.json').json();
